989d5bfe11f5d420e121b9369f8680a077dedf2c
LIMJISEONG <yim2627@gmail.com>
2022-08-05 16:13:32 +0900

    push

--- CI_CD_Practice/ViewController.swift
+++ CI_CD_Practice/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello12312331233ddddwqd")
+        print("Hello12312331233ddddwqdedd")
     }
 
 
