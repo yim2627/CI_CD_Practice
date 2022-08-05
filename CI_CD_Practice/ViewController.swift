dcdb800725643cc5894a6f0ae48c159df1642245
LIMJISEONG <yim2627@gmail.com>
2022-08-05 16:09:34 +0900

    success

--- CI_CD_Practice/ViewController.swift
+++ CI_CD_Practice/ViewController.swift
@@ -13,12 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Hello12312331233ddddwqdd23333")
-        dqwdwqdqwkwkqmqkdmkwqmdkqwmkwqdmwjiqmdjqdmqmdkqmdokqwm
-        ddddddddddd
-        dddd
-        posix_spawn_file_actions_adddup2(<#T##posix_spawn_file_actions_t#>, <#T##Int32#>, <#T##Int32#>)
-        
-        ddddddd
     }
 
 
