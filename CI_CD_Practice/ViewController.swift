81f8924997b0c187ac0d08838683a8247210b33f
LIMJISEONG <yim2627@gmail.com>
2022-08-05 15:31:22 +0900

    dd

--- CI_CD_Practice/ViewController.swift
+++ CI_CD_Practice/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Hello12312331233ddddwqdd23333")
+        dqwdwqdqwkwkqmqkdmkwqmdkqwmkwqdmwjiqmdjqdmqmdkqmdokqwm
     }
 
 
